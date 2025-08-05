2893aece0eae9ba53702d5aa4160fd017b47f2b1
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2025-08-05 15:58:23 -0700

    add SmartWallet (eip7702)

---
+++ src/SmartWallet.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract DelegatedSmartAccount {
+    struct Call {
+        address to;
+        uint256 value;
+        bytes data;
+    }
+
+    function execute(Call[] calldata calls) external payable {
+        for (uint i = 0; i < calls.length; ++i) {
+            (bool success, ) = calls[i].to.call{value: calls[i].value}(
+                calls[i].data
+            );
+            require(success, "call failed");
+        }
+    }
+}
